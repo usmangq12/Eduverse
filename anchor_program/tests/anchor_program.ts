90120d407c4a9daf2d410453d3f8d7bcea45040f
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-05-07 20:38:50 +0500

    fix:removed build issue

--- anchor_program/tests/anchor_program.ts
+++ anchor_program/tests/anchor_program.ts
@@ -1,13 +1,11 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { AnchorProgram } from "../target/types/anchor_program";
-import { describe } from "mocha";
+import { describe, it } from "mocha";
 
 describe("anchor_program", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.anchorProgram as Program<AnchorProgram>;
+  const program = anchor.workspace.anchor_program;
 
   it("Is initialized!", async () => {
     // Add your test here.
---
+++ custom.d.ts
@@ -0,0 +1,6 @@
+declare module '@coral-xyz/anchor';
+declare module '@coral-xyz/anchor/dist/cjs/provider';
+declare module "mocha";
+
+
+
